e45f8eeb4105866f8a3dceadf36e9196ae105843
Martin <martinbruun1995@gmail.com>
2023-12-09 22:07:23 +0100

    Fix layout and add chat functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <script lang="ts">
 </script>
+
+<main>
+	<h1 class="font-bold text-3xl link"><a href="/chat">chat</a></h1>
+</main>
--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,2 +1,16 @@
 <script lang="ts">
 </script>
+
+<main class="h-[calc(100vh-100px)] max-w-[98%] mx-auto grid grid-cols-2">
+	<section class="grid">
+		<section>
+			<input
+				type="text"
+				placeholder="Type here"
+				class="input input-bordered input-primary w-full max-w-xs"
+			/>
+			<button class="btn btn-primary">Send</button>
+		</section>
+	</section>
+	<section></section>
+</main>
